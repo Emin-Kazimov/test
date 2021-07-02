2157be5ae038d58ffe26b58747a351ce0622d492
mkazimov <mammadaminfk@code.edu.az>
2021-07-02 12:59:38 +0400

    second commit

--- src/main/java/com/example/demo2/TestController.java
+++ src/main/java/com/example/demo2/TestController.java
@@ -7,6 +7,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class TestController {
     @GetMapping("/")
     public String test (){
+        System.out.println("sss");
         return "test";
     }
 }
